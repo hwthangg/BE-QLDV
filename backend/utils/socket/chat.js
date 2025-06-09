74f02ae6ad475f678e3d4ec5811222fb7da71fab
tranphuongVy <22521708@gm.uit.edu.vn>
2025-06-09 20:35:56 +0700

    update chat

--- backend/utils/socket/chat.js
+++ backend/utils/socket/chat.js
@@ -6,16 +6,12 @@ export const chat = (io, socket) => {
   const token = socket.handshake.auth?.token;
   const decode = verifyToken(token);
   const userId = decode?.id;
-  socket.on('chat', async(data) => {
+  socket.on('chat', async (data) => {
     console.log(onlineUsers)
     const { partnerId, text } = data
     console.log(partnerId, text)
-    const message = new Message({
-      members: [userId, partnerId],
-      sender:userId,
-      text:text
-    });
 
-    await message.save();
+    io.to(onlineUsers[partnerId]).emit('chat', ({ id: '', text: text, sender: userId }))
+
   })
 }
